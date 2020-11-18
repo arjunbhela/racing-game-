2c3ee0fc12a45e55020459c35fad32c469d985ac
arjunbhela <71056419+arjunbhela@users.noreply.github.com>
2020-11-17 19:56:07 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var database;
+var playerCount = 0;
+var gameState = 0;
+var form,game,player;
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+  game = new Game()
+  game.getState();
+  game.start();
+}
+
+function draw(){
+  background("white");
+  
+}
